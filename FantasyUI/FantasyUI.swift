56a4582c739d43028d6ec0a0907ea0bbffaa6713
liseami <51239614+liseami@users.noreply.github.com>
2021-11-19 22:06:19 +0800

    commit

---
+++ FantasyUI/FantasyUI.swift
@@ -0,0 +1,14 @@
+//
+//  FantasyUI.swift
+//  FantasyUI
+//
+//  Created by Liseami on 2021/11/19.
+//
+
+import SwiftUI
+
+struct FantasyUI: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
