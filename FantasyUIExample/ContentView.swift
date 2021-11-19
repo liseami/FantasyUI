b32e01dfa46cc5fe7cd8f4066f220d4f5c4e456c
liseami <51239614+liseami@users.noreply.github.com>
2021-11-19 21:36:47 +0800

    commit

---
+++ FantasyUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FantasyUIExample
+//
+//  Created by Liseami on 2021/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FantasyUIExample/FantasyUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyUIExampleApp.swift
+//  FantasyUIExample
+//
+//  Created by Liseami on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct FantasyUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
