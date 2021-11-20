de7c4dbff2e722a8b98c07de9fd3c113966e5e83
liseami <51239614+liseami@users.noreply.github.com>
2021-11-20 16:10:11 +0800

    commit

--- FantasyUI/NavigationTools.swift
+++ FantasyUI/NavigationTools.swift
@@ -7,11 +7,25 @@
 
 import SwiftUI
 
-extension View{
+#if canImport(AppKit)
+import AppKit
+#elseif canImport(UIKit)
+import UIKit
+#endif
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15.0, *)
+extension View {
+    public func inject<SomeView>(_ view: SomeView) -> some View where SomeView: View {
+        return background(view)
+    }
+}
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15.0, *)
+extension View {
     
     ///封装系统Navilink
-    public func PF_Navilink<Link>(isPresented: Binding<Bool>,  @ViewBuilder content: @escaping () -> Link ) -> some View where Link : View{
-        let result = self.background(
+     public func PF_Navilink<Link>(isPresented: Binding<Bool>, content: @escaping () -> Link ) -> some View where Link : View{
+         return inject(
             ZStack{
                 Group{
                     NavigationLink(isActive: isPresented) {
@@ -28,33 +42,30 @@ extension View{
                 }
             }
         )
-        return result
+        
     }
     
     ///封装系统Sheet
     ///解决iOS14.4出现的单一响应问题：一个View多个Sheet只有最后一个响应
-    public func PF_SystemSheet<Content>(isPresented: Binding<Bool>, onDismiss: (()->Void)?, @ViewBuilder content: @escaping () -> Content) -> some View where Content : View{
-        let result = self.background(
-            Text("金诚所至，金石为开")
-                .opacity(0)
-                .sheet(isPresented: isPresented, onDismiss: onDismiss, content: {
-                    content()
-                })
-        )
-        return result
+     public func PF_SystemSheet<Content>(isPresented: Binding<Bool>, onDismiss: (()->Void)?,  content: @escaping () -> Content) -> some View where Content : View{
+         
+        return inject(Text("金诚所至，金石为开")
+                        .opacity(0)
+                        .sheet(isPresented: isPresented, onDismiss: onDismiss, content: {
+                            content()
+                        }))
     }
     
     ///封装系统FullScreen
     ///解决iOS14.4出现的单一响应问题：一个View多个Sheet只有最后一个响应
-    public func PF_FullScreen<Content>(isPresented: Binding<Bool>,  onDismiss: (()->Void)?, @ViewBuilder content: @escaping () -> Content) -> some View where Content : View{
-        let result = self.background(
-            Text("金诚所至，金石为开")
-                .opacity(0)
-                .fullScreenCover(isPresented: isPresented, onDismiss: onDismiss, content: {
-                    content()
-                })
-        )
-        return result
+     public func PF_FullScreen<Content>(isPresented: Binding<Bool>,  onDismiss: (()->Void)?, content: @escaping () -> Content) -> some View where Content : View{
+         return inject(Text("金诚所至，金石为开")
+                        .opacity(0)
+                        .fullScreenCover(isPresented: isPresented, onDismiss: onDismiss, content: {
+                            content()
+                        }))
+        
     }
     
 }
+
