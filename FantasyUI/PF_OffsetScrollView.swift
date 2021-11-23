51634063955efb9d5b134a84879a6a16b8df23be
liseami <51239614+liseami@users.noreply.github.com>
2021-11-23 15:12:30 +0800

    commit in submodule

---
+++ FantasyUI/PF_OffsetScrollView.swift
@@ -0,0 +1,72 @@
+//
+//  OffsetScrollView.swift
+//  Cashmix
+//
+//  Created by Liseami on 2021/11/18.
+//
+
+import SwiftUI
+
+//可以测算offset的ScrollowView
+public struct PF_OffsetScrollView<Body> : View  where Body : View{
+    
+    
+    @Binding var offset : CGFloat
+    let content : ()-> Body
+   
+    
+    
+    public init(offset:Binding<CGFloat>,content : @escaping ()-> Body) {
+        _offset = offset
+        self.content = content
+    }
+    
+    
+    // MARK: - Body
+    @ViewBuilder
+    public var body: some View{
+        
+        ScrollView(.vertical, showsIndicators: false)  {
+            offsetDetector
+            
+            self.content()
+                .padding(.top,TopSafeArea + 44)
+            
+            Spacer().frame(width: 0, height: SW)
+        }
+        .ignoresSafeArea()
+        
+    }
+    
+    @ViewBuilder
+    var offsetDetector : some View{
+        // Header View...
+        GeometryReader{ proxy -> AnyView in
+            // Sticky Header...
+            let minY = proxy.frame(in: .global).minY
+            DispatchQueue.main.async {
+                self.offset = minY
+            }
+            return AnyView(
+                ZStack{
+                    Divider().opacity(0)
+                        .offset(y:(TopSafeArea))
+                }
+            )
+        }
+        .frame(height: 0)
+        .zIndex(1)
+    }
+}
+
+
+struct PF_OffsetScrollView_OffsetScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        PF_OffsetScrollView(offset: .constant(32), content: {
+            ForEach(0 ..< 5) { item in
+                Text("23")
+            }
+        })
+    }
+}
---
+++ FantasyUI/PF_Sheet.swift
@@ -0,0 +1,171 @@
+//
+//  PFSheet.swift
+//  Cashmix
+//
+//  Created by Liseami on 2021/11/19.
+//
+
+import SwiftUI
+
+struct PF_SheetView<Content,Back> : View where Content : View ,Back:View{
+    @Environment(\.colorScheme) var colorScheme
+    let content : ()->Content
+    let background : ()->Back
+    
+    @GestureState var offset : CGFloat = 0
+    @State private var bodyHeight : CGFloat = 0
+    @Binding var show : Bool
+    var capsulebarColor : Color = .black
+    var backcornerRadius : CGFloat = 32
+    
+    init(isPresented : Binding<Bool>,capsulebarColor : Color = .black , backcornerRadius : CGFloat = 32, content:@escaping ()-> Content,background:@escaping ()-> Back)
+    {
+        _show = isPresented
+        self.content = content
+        self.background = background
+        self.capsulebarColor = capsulebarColor
+        self.backcornerRadius = backcornerRadius
+        
+    }
+    
+    @ViewBuilder
+    var body: some View {
+        
+        
+        //手势
+        let gesture = DragGesture(minimumDistance: 4, coordinateSpace: CoordinateSpace.global)
+            .updating($offset, body: { value, out , transition in
+                out = value.translation.height
+            })
+            .onEnded({ value in
+                if value.translation.height > (bodyHeight * 0.2){
+                    madasoft()
+                    withAnimation(.spring()){
+                        show = false
+                    }
+                }
+            })
+        
+        
+        VStack
+        {
+            Spacer()
+            capsulebar
+            self.content()
+                .background(back)
+                .offset(y:offset > 0 ? offset : 0)
+                .gesture(gesture)
+        }
+        
+        
+    }
+    @ViewBuilder
+    var back : some View{
+        GeometryReader(content: { proxy in
+            background()
+                .frame(width: SW)
+                .clipShape(RoundedCorner(radius: backcornerRadius, corners: [.topLeft,.topRight]))
+                .ignoresSafeArea()
+                .onAppear {
+                    self.bodyHeight = proxy.size.height
+                }
+        })
+    }
+    
+    var capsulebar : some View {
+        
+        Capsule(style: .continuous)
+            .frame(width: SW * 0.1, height: 4)
+            .foregroundColor(capsulebarColor)
+            .offset(y:offset > 0 ? offset : 0)
+        
+    }
+    
+    
+}
+
+
+extension View{
+    
+    public func PF_Sheet<Content,Back>(isPresented: Binding<Bool>, capsulebarColor : Color = .black,backcornerRadius:CGFloat = 32, @ViewBuilder content: @escaping () -> Content,@ViewBuilder background: @escaping () -> Back) -> some View where Content : View,Back:View{
+        
+        
+        
+        return  self
+            .overlay(
+                ZStack{
+                    Color.black.opacity(0.7).ignoresSafeArea()
+                        .onTapGesture {
+                            withAnimation(){
+                                madasoft()
+                                isPresented.wrappedValue = false
+                            }
+                        }
+                        .ifshow(isPresented.wrappedValue, animation: .linear(duration: 0.4), transition: .opacity)
+                    
+                    //抽屉
+                    PF_SheetView(isPresented: isPresented,capsulebarColor: capsulebarColor, backcornerRadius: backcornerRadius, content: content, background: background)
+                        .ifshow(isPresented.wrappedValue, animation: .linear(duration: 0.4), transition: .move(edge: .bottom))
+                }
+            )
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
+
+struct PF_SheetViewExample: View {
+    @State private var show : Bool = false
+    var body: some View {
+        
+        ZStack{
+            NavigationView{
+                Color.yellow
+                    .navigationTitle("The Big Blue")
+            }
+            Button {
+                show.toggle()
+            } label: {
+                Text("show Sheet")
+            }
+        }
+        .PF_Sheet(isPresented: $show, capsulebarColor: .black) {
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("Hello,world")
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("Hello,world")
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("Hello,world")
+                    Spacer()
+                }
+                
+            }
+            
+        } background: {
+            Color.red
+        }
+        
+    }
+}
+
+struct PF_SheetViewExample_Previews: PreviewProvider {
+    static var previews: some View {
+        PF_SheetViewExample()
+    }
+}
